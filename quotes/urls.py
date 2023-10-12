e4388931fd7c913662bfc7b49105521fe084f906
joebaga <jlirenge@gmail.com>
2023-10-12 20:26:38 +0300

    장고에서 프로젝트를 만들고 html 파일을 추가했습니다

---
+++ quotes/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index, name='index'),
+    path('quotes',views.quotes, name='quotes'),
+    path('config',views.config, name='config')
+]
