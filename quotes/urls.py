50b82791de245516edbd119b85aa8ddeff20ed41
joebaga <jlirenge@gmail.com>
2023-11-07 07:09:25 +0300

    home page and user login/logout tmple

--- quotes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.index, name='index'),
-    path('quotes/',views.quotes, name='quotes'),
-    path('config/',views.config, name='config')
-]
