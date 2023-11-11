e7082df9265d57af1c04c6f754eb1942508dc2f0
andrewjun123 <ehdgns1475@hufs.ac.kr>
2023-11-11 21:20:48 +0900

    add order, price, info

--- quotes/urls.py
+++ quotes/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('home/',views.index, name='index'),
     path('quotes/',views.quotes, name='quotes-page'),
+    path('price/',views.price, name='price-page'),
+    path('order/',views.order, name='order-page'),
+    #path('info/',views.price, name='info-page'),
     path('config/',views.config, name='config')
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -12,6 +12,24 @@ def index(request):
 def quotes(request):
     return render(request, 'Home/quotes.html')
 
+
+#Price 추가
+@login_required(login_url='user-login')
+def price(request):
+    return render(request, 'Home/price.html')
+
+
+#Order 추가
+@login_required(login_url='user-login')
+def order(request):
+    return render(request, 'Home/order.html')
+
+
+# #Info 추가
+# @login_required(login_url='user-login')
+# def price(request):
+#     return render(request, 'Home/info.html')
+
 @login_required(login_url='user-login')
 def config(request):
     return render(request, 'Home/conf.html')
